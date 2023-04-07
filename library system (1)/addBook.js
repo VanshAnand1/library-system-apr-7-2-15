c63218378e9f9a07325112b40fb59cea1bbefafa
VanshAnand1 <130173778+VanshAnand1@users.noreply.github.com>
2023-04-07 13:23:46 -0400

    Initial commit

---
+++ library system (1)/addBook.js
@@ -0,0 +1,27 @@
+/*
+function test() {
+    alert('The book has been added!');
+}
+*/
+
+let newBookTitle;
+let newBookNum;
+let newBookAuthor;
+let newBookType;
+let newBookPub;
+let newBookGenre;
+
+function addNewBook(addBook) {
+    let newBookNum = addbook.bookNumL.value;
+    alert(newBookNum);
+    let newBookTitle = addbook.titleL.value;
+    alert(newBookTitle);
+    let newBookAuthor = addbook.bookNumL.value;
+    alert(newBookAuthor);
+    let newBookType = addbook.authorL.value;
+    alert(newBookType);
+    let newBookPub = addbook.bookTypeL.value;
+    alert(newBookPub);
+    let newBookGenre = addbook.pubDateL.value;
+    alert(newBookGenre);
+}
